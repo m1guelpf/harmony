4eb90d4456b911d72341c4e3f21d31be00922250
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-08-06 20:35:41 +0200

    wip

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,18 +2,28 @@ import Head from 'next/head'
 import { usePageLayout } from '../components/PageLayout'
 import { WithAuth } from '../middleware/auth'
 import { useSession } from 'next-auth/client'
+import Link from 'next/link'
 
 const Home = () => {
 	const [session] = useSession()
 
-	console.log(session)
 	return (
 		<div className="container">
 			<Head>
 				<title>Harmony</title>
 			</Head>
 
-			<div>Welcome to Harmony</div>
+			{session ? (
+				<div>
+					WIP. Go to{' '}
+					<Link href="/[username]" as={`/${session.user.username}`}>
+						<a className="underline">your profile</a>
+					</Link>
+					.
+				</div>
+			) : (
+				<span>Wait a second</span>
+			)}
 		</div>
 	)
 }
