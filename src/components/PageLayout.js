d69d7396772ac3b23318556252bcbbb62ca263c6
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-08-07 07:13:37 +0200

    Fix login icon appearance

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -52,7 +52,7 @@ const PageLayout = ({ children, ...pageProps }) => {
 							</div>
 						) : (
 							<Link href="/login">
-								<a className="text-center mr-2 px-4 my-3 border border-transparent text-sm leading-5 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-50 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-indigo-200 transition ease-in-out duration-150">Log In</a>
+								<a className="text-center mr-2 px-4 my-3 flex items-center border border-transparent text-sm leading-5 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-50 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-indigo-200 transition ease-in-out duration-150">Log In</a>
 							</Link>
 						)}
 					</div>
