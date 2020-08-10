05bfdf9d894e1c592b9c678acde5e472104552bc
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-08-10 09:43:10 +0200

    Add title to all pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,6 +11,7 @@ const MyApp = ({ Component, pageProps }) => {
 	return (
 		<>
 			<Head>
+				<title>Harmony</title>
 				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
 			</Head>
 			{getLayout(<Component {...pageProps} />, { ...pageProps, middleware: Component.middleware ?? [] })}
