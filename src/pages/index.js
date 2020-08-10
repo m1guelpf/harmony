d2b96e27c6990d7328a2f2def0d4961edaf479fb
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-08-10 09:48:20 +0200

    make it look nicer on desktop

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Client from '../utils/client'
 import Skeleton from '../components/Skeleton'
 
 const Home = () => {
-	let { data: profiles } = useSWR('/api/profiles', () => Client.profiles())
+	const { data: profiles } = useSWR('/api/profiles', () => Client.profiles())
 
 	return (
 		<div className="container">
@@ -13,7 +13,7 @@ const Home = () => {
 				<div className="flex items-center justify-between mb-2">
 					<p className="text-xl font-medium dark:text-gray-300">Users</p>
 				</div>
-				<div className="grid gap-4 grid-cols-3 grid-flow-row">{profiles ? profiles.map(({ id, name, avatar }) => <Profile key={id} username={id} name={name} avatar={avatar} />) : [...Array(10).keys()].map((id) => <Profile key={id} />)}</div>
+				<div className="grid gap-4 grid-cols-3 sm:grid-cols-5 grid-flow-row">{profiles ? profiles.map(({ id, name, avatar }) => <Profile key={id} username={id} name={name} avatar={avatar} />) : [...Array(10).keys()].map((id) => <Profile key={id} />)}</div>
 			</div>
 		</div>
 	)
@@ -24,7 +24,7 @@ const Profile = ({ username, name, avatar }) => {
 
 	return (
 		<LinkOrDiv href="/[username]" as={`/${username}`}>
-			<a>
+			<a className="flex flex-col items-start sm:mb-4">
 				{avatar ? <div className="bg-white p-4 rounded-lg w-24 h-24 bg-cover" style={{ backgroundImage: `url(${avatar})` }} /> : <Skeleton className="w-24 h-24" />}
 				<p className="mt-1 text-center text-sm dark:text-gray-400">{name || <Skeleton />}</p>
 			</a>
